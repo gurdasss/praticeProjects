75ba04a5d219c06422e2d89e59a269a6bd5622e3
Gurdas Satwani <gurdassatwani1@gmail.com>
2024-09-24 06:13:39 +0530

    updates

--- flappy-bird/scripts/bird.gd
+++ flappy-bird/scripts/bird.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,13 +11,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and position.y < 100:
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		animated_sprite.play("fly")
-		print(position.y)
-	
-	if is_on_floor():
-		floor_snap_length = 0.0
-		animated_sprite.stop()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
