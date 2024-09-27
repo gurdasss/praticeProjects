2205b8e1d29833e8b525c866969424dd41f7d6bd
Gurdas Satwani <137665724+gurdasss@users.noreply.github.com>
2024-09-27 14:40:34 +0530

    Add files via upload

--- flappy-bird/scripts/main.gd
+++ flappy-bird/scripts/main.gd
@@ -4,6 +4,7 @@ var pipeScene = preload("res://scenes/pipe.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_child(pipeScene.instantiate())
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- flappy-bird/scripts/pipe.gd
+++ flappy-bird/scripts/pipe.gd
@@ -1,9 +1,12 @@
 extends StaticBody2D
 
-
-
+var randomNum: float = 0.0
+@export var maxOffset: float = 150.00
+@export var minOffset: float = 0.00
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x = 50
+	randomNum = randf_range(minOffset, maxOffset)
+	position.y = randomNum
+	print(randomNum)
