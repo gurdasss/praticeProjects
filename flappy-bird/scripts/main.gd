6cffa4e71fabd887800965fe83597584b8742aa5
Gurdas Satwani <gurdassatwani1@gmail.com>
2024-09-28 14:00:13 +0530

    updates

--- flappy-bird/scripts/main.gd
+++ flappy-bird/scripts/main.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
 var pipeScene = preload("res://scenes/pipe.tscn")
+
+func generatePipes():
+	var pipeObj: Node = pipeScene.instantiate()
+	var rMinOffset: float = randf_range(pipeObj.get("MIN_OFFSET"), pipeObj.get("MAX_OFFSET"))
+	var rMaxOffset: float = randf_range(pipeObj.get("MIN_OFFSET"), pipeObj.get("MAX_OFFSET"))
+	add_child(pipeObj)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_child(pipeScene.instantiate())
-	pass
-
-
+	generatePipes()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
--- flappy-bird/scripts/pipe.gd
+++ flappy-bird/scripts/pipe.gd
@@ -1,12 +1,16 @@
-extends StaticBody2D
+extends Node2D
 
-var randomNum: float = 0.0
-@export var maxOffset: float = 150.00
-@export var minOffset: float = 0.00
+const MAX_OFFSET: float = 200.00
+const MIN_OFFSET: float = 150.00
 
-
+func _ready() -> void:
+	pass
+   
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	randomNum = randf_range(minOffset, maxOffset)
-	position.y = randomNum
-	print(randomNum)
+	# destroy a pipe instance once they are out of the main window.
+	if position.x <= -240:
+		queue_free()
+		print("Pipe was destryoed")
+
+	position.x -= 1
